35587929135872d911419367e2754d1cc4ec6a43
maxbl4 <maxbl4@gmail.com>
2014-02-07 23:20:27 +0400

    Added forgotten files

---
+++ AutoRu.CrawlerSvc/CrawlerService.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Diagnostics;
+using System.ServiceProcess;
+using System.Threading;
+using AutoRu.Common;
+
+namespace AutoRu.CrawlerSvc
+{
+    public partial class CrawlerService : ServiceBase
+    {
+        public const string LogName = "AutoRuCrawler";
+        private Crawler crawler;
+        private Thread schedulerThread;
+        private bool serviceIsStopping = false;
+        private DateTime lastCrawl = DateTime.MinValue;
+        private EventLog log;
+        public CrawlerService()
+        {
+            InitializeComponent();
+        }
+
+        public void Start()
+        {
+            OnStart(null);
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            log = new EventLog(LogName);
+            log.Source = LogName;
+            log.WriteEntry("Starting");
+            Global.Initialize("mongodb://mx2");
+            crawler = new Crawler();
+            schedulerThread = new Thread(Scheduler);
+            schedulerThread.Start();
+        }
+
+        private void Scheduler()
+        {
+            while (!serviceIsStopping)
+            {
+                if ((DateTime.Now - lastCrawl).TotalSeconds > 60)
+                {
+                    DoCrawlCycle();
+                    lastCrawl = DateTime.Now;
+                }
+                Thread.Sleep(5000);
+            }
+        }
+
+        private void DoCrawlCycle()
+        {
+            try
+            {
+                if (serviceIsStopping) return;
+                crawler.CrawlOnce();
+                log.WriteEntry("Done crawling");
+            }
+            catch (Exception ex)
+            {
+                log.WriteEntry("Error in crawling: " + ex.ToString());
+            }
+        }
+
+        protected override void OnStop()
+        {
+            serviceIsStopping = true;
+            log.WriteEntry("Stopping");
+        }
+    }
+}
---
+++ AutoRu.CrawlerSvc/ProjectInstaller.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoRu.CrawlerSvc
+{
+    [RunInstaller(true)]
+    public partial class ProjectInstaller : System.Configuration.Install.Installer
+    {
+        public ProjectInstaller()
+        {
+            InitializeComponent();
+        }
+
+        public override void Install(IDictionary stateSaver)
+        {
+            base.Install(stateSaver);
+
+            if (System.Diagnostics.EventLog.SourceExists(CrawlerService.LogName))
+                System.Diagnostics.EventLog.CreateEventSource(CrawlerService.LogName, CrawlerService.LogName);
+        }
+
+        public override void Uninstall(IDictionary savedState)
+        {
+            if (System.Diagnostics.EventLog.SourceExists(CrawlerService.LogName))
+                System.Diagnostics.EventLog.DeleteEventSource(CrawlerService.LogName);
+            base.Uninstall(savedState);
+        }
+    }
+}
