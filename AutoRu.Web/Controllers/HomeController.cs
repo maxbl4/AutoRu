c82f5f7623aba43ea13d85b137499aa612bf430e
maxbl4 <maxbl4@gmail.com>
2014-04-28 21:56:59 +0400

    Начал писать алгоритм показа сообщений

--- AutoRu.Web/Controllers/HomeController.cs
+++ AutoRu.Web/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
 using AutoRu.Common;
+using Microsoft.AspNet.Identity;
 using MongoDB.Driver.Linq;
 
 namespace AutoRu.Web.Controllers
@@ -21,6 +22,17 @@ namespace AutoRu.Web.Controllers
 
         public ActionResult Forum(string id)
         {
+            var readIds = new HashSet<int>();
+            var userName = User.Identity.GetUserName();
+            if (!string.IsNullOrEmpty(userName))
+            {
+                var user = Global.Db.User.AsQueryable().FirstOrDefault(x => x.UserName == userName);
+                if (user != null)
+                {
+                    readIds.UnionWith(Global.Db.ReadId.AsQueryable().Where(x => x.UserId == user.Id).Select(x => x.PostId));
+                }
+            }
+
             ViewBag.ForumId = id;
             var posts = Global.Db.Post.AsQueryable()
                 .Where(x => x.ForumId == id)
--- AutoRu/Program.cs
+++ AutoRu/Program.cs
@@ -17,8 +17,8 @@ namespace AutoRu
     {
         static void Main(string[] args)
         {
-            new Crawler().CrawlOnce();
-            return;
+            //new Crawler().CrawlOnce();
+            //return;
             var user = Global.Db.User.AsQueryable().FirstOrDefault(x => x.UserName == "Max");
             if (user == null)
             {
@@ -26,18 +26,62 @@ namespace AutoRu
                 Global.Db.User.Save(user);
             }
 
-            var cufoffDate = new DateTime(2014, 03, 26);
+            var cufoffDate = new DateTime(2014, 04, 26);
 
             var readPosts = new HashSet<int>(Global.Db.ReadId.AsQueryable()
                 .Where(x => x.Timestamp > cufoffDate)
                 .Select(x => x.PostId));
-            var newPosts = Global.Db.Post.AsQueryable()
-                .Where(x => x.ForumId == "moto" 
-                    && x.Timestamp > cufoffDate
-                    && !readPosts.Contains(x.Id))
-                .OrderByDescending(x => x.TopicId)
-                .ThenBy(x => x.Index)
+            //var newPosts = Global.Db.Post.AsQueryable()
+            //    .Where(x => x.ForumId == "moto" 
+            //        && x.Timestamp > cufoffDate
+            //        && !readPosts.Contains(x.Id))
+            //    .OrderByDescending(x => x.TopicId)
+            //    .ThenBy(x => x.Index)
+            //    .ToList();
+            var posts = Global.Db.Post.AsQueryable()
+                .Where(x => x.TopicId == 2529537)
+                .OrderBy(x => x.Index)
                 .ToList();
+            var readIds = new HashSet<int>{
+                2529548,
+2529551,
+2529582,
+2529605,
+2529585,
+2529593,
+2529591,
+            };
+            for (int i = 0; i < posts.Count; i++)
+            {
+                var post = posts[i];
+                if (readIds.Contains(post.Id))
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+                var skipPost = false;
+                if (i > 0 && i < posts.Count - 1)
+                {
+                    var prevPost = posts[i - 1];
+                    var nextPost = posts[i + 1];
+                    if (readIds.Contains(prevPost.Id)
+                        && readIds.Contains(post.Id)
+                        && readIds.Contains(nextPost.Id))
+                    {
+                        skipPost = true;
+                    }
+                }
+                if (skipPost != true)
+                    Console.WriteLine("{1} {0}{2}", new String(' ', post.Offset), post.Id, post.Title);
+                else
+                {
+                    //Console.WriteLine("...");
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
